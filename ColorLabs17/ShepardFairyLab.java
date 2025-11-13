e2984cc04e4d81616395a9fdd33dd5a1a1e64367
jblach-ph <jblach@stu.naperville203.org>
2025-11-13 08:29:11 -0600

    more shepard fairy
    narrow white down to get more definition on face for method 2

--- ColorLabs17/ShepardFairyLab.java
+++ ColorLabs17/ShepardFairyLab.java
@@ -17,88 +17,102 @@ public class ShepardFairyLab
      */
     public static void main(String[] args)
     {
+        //change with selfie picture
+        Picture j1 = new Picture("images/justin selfie.jpg");
+        Picture gorge = new Picture("images/gorge.jpg");
+        Picture j2 = new Picture("images/justin selfie.jpg");
+        /**
+         * method 1 change
+         * 
+         */
+        int red, blue, green;
+        Color dkblue = new Color(0, 0, 139);
+        Color ltsteel = new Color(176, 196, 222);
+        Color Red = new Color(255, 0, 0);
+        Color bone = new Color(227, 218, 201);
+        Color offwt = new Color(242, 240, 239);
         
-         //opens selfie picture 
-          /**/
-         String fileName = FileChooser.pickAFile();
-         Picture pictObj = new Picture(fileName);
-         pictObj.explore();
-         
-         //relative path
-         Picture j = new Picture("images\\justin selfie.jpg");
-         //change with selfie picture
-         Picture j1 = new Picture("images/justin selfie.jpg");
-         Picture j2 = new Picture("images/justin selfie.jpg");
-         Picture j3 = new Picture("images/justin selfie.jpg");
-         /**
-          * method 1 change
-          * 
-          */
-         int red, blue, green;
-         Pixel[] pixelJ;
-         pixelJ = j1.getPixels();
-         Color dkblue = new Color(0, 0, 139);
-         Color ltsteel = new Color(176, 196, 222);
-         Color Red = new Color(255, 0, 0);
-         Color bone = new Color(227, 218, 201);
-         Color offwt = new Color(242, 240, 239);
-         j1.explore();
-         for (Pixel spot1 : pixelJ)
-         {
-             red = spot1.getRed();
-             blue = spot1.getBlue();
-             green = spot1.getGreen();
-             int avg = (red + blue + green) / 3;
-             spot1.setRed(avg);
-             spot1.setBlue(avg);
-             spot1.setGreen(avg);
-             
-             if (red < 70 && blue < 70 && green < 70)
-             {
-                 spot1.setColor(dkblue);
-             }
-             else if ((red >= 70 && red < 128) && (blue >= 70 && blue < 128) && (green >= 70 && green < 128))
-             {
-                 spot1.setColor(Red);
-             }
-             else if ((red >= 128 && red < 175) && (blue >= 128 && blue < 175) && (green >= 128 && green < 175))
-             {
-                 spot1.setColor(ltsteel);
-             }
-             else if ((red >= 150 && red < 200) && (blue >= 150 && blue < 200) && (green >= 150 && green < 200))
-             {
-                 spot1.setColor(bone);
-             }
-             else
-             {
-                 spot1.setColor(offwt);
-             }
-             
-             // add a fifth bucket for more detail in face
-         }
-         j1.explore();
-         
-         // group 1 (Dark blue)
-         
-         
-         // group 2 (Red)
-         
-         
-         // group 3 (Light blue)
-         
-         
-         // group 4 (Off-white)
-         
-         
-         /**
-          * method 2 change
-          * 
-          */
-         
-         /**
-          * custom color palette
-          */
-
-         
+        // selfie (method 1)
+        Pixel[] pixelJ;
+        pixelJ = j1.getPixels();
+        for (Pixel spot1 : pixelJ)
+        {
+            red = spot1.getRed();
+            blue = spot1.getBlue();
+            green = spot1.getGreen();
+            int avg = (red + blue + green) / 3;
+            spot1.setRed(avg);
+            spot1.setBlue(avg);
+            spot1.setGreen(avg);
+            if (red < 65)
+            {
+                spot1.setColor(dkblue);
+            }
+            else if (red < 100)
+            {
+                spot1.setColor(Red);
+            }
+            else if (red < 138)
+            {
+                spot1.setColor(ltsteel);
+            }
+            else if (red < 194)
+            {
+                spot1.setColor(bone);
+            }
+            else
+            {
+                spot1.setColor(offwt);
+            }
+        }
+        j1.explore();
+        
+        // gorge (method 2) narrow white down to get more definition on face
+        Pixel[] pixelJ2;
+        pixelJ2 = j2.getPixels();
+        int s = 255;
+        int b = 0;
+        for (Pixel spot2 : pixelJ2)
+        {
+            red = spot2.getRed();
+            blue = spot2.getBlue();
+            green = spot2.getGreen();
+            int avg = (red + blue + green) / 3;
+            spot2.setRed(avg);
+            spot2.setBlue(avg);
+            spot2.setGreen(avg);
+            
+            if (red > b)
+            {
+                b = red;
+            }
+            if (red < s)
+            {
+                s = red;
+            }
+            int Avg = (b - s) / 4;
+            if (red < Avg)
+            {
+                spot2.setColor(dkblue);
+            }
+            else if (red < (2 * Avg))
+            {
+                spot2.setColor(Red);
+            }
+            else if (red < (3 * Avg))
+            {
+                spot2.setColor(ltsteel);
+            }
+            else
+            {
+                spot2.setColor(offwt);
+            }
+        }
+        System.out.println(s + " " + b);
+        j2.explore();
+        /**
+         * custom color palette
+         */
+        
     }//main       
 }//class
